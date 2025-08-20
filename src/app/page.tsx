fe54d5bff9524afc191969a869d8e8426b6653b9
Heng Mouyleng <mouylengh@HMLs-Mac-mini.local>
2025-08-20 11:31:21 +0700

    feat(setup): add husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -14,7 +14,6 @@ export default function Home() {
           priority
         />
         <Button>Click me</Button>
-
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
